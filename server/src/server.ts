c1e712a1f2a04976171c0b8da050acb129ec0179
Francelino <fmarcilio1989@gmail.com>
2020-08-24 17:20:08 -0300

    conecção com o banco

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello word');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    
+    return response.json({ message:'Hello Word' });
+});
+
+app.listen(3333)
