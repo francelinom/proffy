93c2bba9bb75da1cb2fc880dd1fd72bee829ca5a
Francelino <fmarcilio1989@gmail.com>
2020-08-24 22:55:07 -0300

    criando as routes

---
+++ server/src/routes.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+routes.post('/classes', async (request, response) => { 
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+
+    const insertedusersIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+    });
+
+    const user_id = insertedusersIds[0];
+
+    const insertedClassesids = await db('classes').insert({
+        subject,
+        cost,
+        user_id
+    });
+
+    const class_id = insertedClassesids[0];
+
+    
+
+    return response.send();
+});
+
+export default routes;
